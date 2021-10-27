896ff99e0b3ba5a65e554d134fcb14206a5a7c7e
Jmedrado1962 <92185447+Jmedrado1962@users.noreply.github.com>
2021-10-27 18:51:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res)=>{
     res.status(200).send({message: 'Olá mundo estou funcionando'});
 })
 
+app.get('/', (req, res)=>{
+    res.status(200).send({message: 'Oi, eu sou a rota Olá'});
+})
+
 app.listen(3001, ()=>{
     console.log('api rodando na porta 3001');
 })
